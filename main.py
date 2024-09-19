a694676ca96e9dff8d9be4fa338a84b659bae8c0
Brandon <135168251+MrJiagge@users.noreply.github.com>
2024-09-19 11:51:54 +0100

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+player_details = {
+    "username": str,
+    "password": str,
+    "email": str,
+    "phone_number": int,
+    "age": int,
+    "credit_card_number": str,
+    "date_of_brith": str
+}
+
+valid_username = False
+valid_password = False
+valid_email = False
+valid_phone_number = False
+valid_age = False
+valid_credit_card_number = False
+valid_date_of_birth = False
+
+test = player_details["username"]
+test = "test"
+print(test)
